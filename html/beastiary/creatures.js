37c0be500b542432dbbbc25a044fb3a69d09382e
draedonlog <128853744+draedonlog@users.noreply.github.com>
2023-04-07 12:48:30 -0400

    Implemented rough beastiary

---
+++ html/beastiary/creatures.js
@@ -0,0 +1,517 @@
+let creatures = '{"characters":[' +
+		// Artemis
+		'{' +
+			'"name":"Artemis",' +
+			'"file":"artemis",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Jungle",' +
+			'"race":"Half Elf",' +
+			'"desc":"bruh"' +
+		'},' +
+
+		// Ashley
+		'{' +
+			'"name":"Ashley",' +
+			'"file":"ashley",' +
+			'"fact":"unaligned",' +
+			'"status":"Alive",' +
+			'"origin":"Barindale",' +
+			'"race":"Human",' +
+			'"desc":""' +
+		'},' +
+
+		// Aurora
+		'{' +
+			'"name":"Aurora",' +
+			'"file":"aurora",' +
+			'"fact":"pyranus",' +
+			'"status":"Deceased",' +
+			'"origin":"Unknown",' +
+			'"race":"Celestial",' +
+			'"desc":""' +
+		'},' +
+
+		// Bart
+		'{' +
+			'"name":"Bart",' +
+			'"file":"bart",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Barindale",' +
+			'"race":"Human",' +
+			'"desc":""' +
+		'},' +
+
+		// Book
+		'{' +
+			'"name":"Book",' +
+			'"file":"book",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Lothra",' +
+			'"race":"Lizardfolk",' +
+			'"desc":""' +
+		'},' +
+
+		// Borg
+		'{' +
+			'"name":"Borg",' +
+			'"file":"borg",' +
+			'"fact":"demanatus",' +
+			'"status":"Deceased",' +
+			'"origin":"Rotha",' +
+			'"race":"Orc",' +
+			'"desc":""' +
+		'},' +
+
+		// Cultist
+		'{' +
+			'"name":"Void Cultist",' +
+			'"file":"cultist",' +
+			'"fact":"unaligned",' +
+			'"status":"Deceased",' +
+			'"origin":"Jungle",' +
+			'"race":"Human",' +
+			'"desc":""' +
+		'},' +
+
+		// Cunter Nickel
+		'{' +
+			'"name":"Cunter Nickel",' +
+			'"file":"cunter-nickel",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Triven",' +
+			'"race":"Warforged",' +
+			'"desc":""' +
+		'},' +
+
+		// Daedalus
+		'{' +
+			'"name":"Daedalus",' +
+			'"file":"daedalus",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Marione",' +
+			'"race":"Elven Demigod",' +
+			'"desc":""' +
+		'},' +
+
+		// Daram
+		'{' +
+			'"name":"Daram",' +
+			'"file":"daram",' +
+			'"fact":"pyranus",' +
+			'"status":"Alive",' +
+			'"origin":"Unknown",' +
+			'"race":"Celestial",' +
+			'"desc":""' +
+		'},' +
+
+		// Demanatus
+		'{' +
+			'"name":"Demanatus",' +
+			'"file":"demanatus",' +
+			'"fact":"demanatus",' +
+			'"status":"Alive",' +
+			'"origin":"Jungle",' +
+			'"race":"Half Elf",' +
+			'"desc":""' +
+		'},' +
+
+		// Devourer
+		'{' +
+			'"name":"Devourer",' +
+			'"file":"devourer",' +
+			'"fact":"demanatus",' +
+			'"status":"Alive",' +
+			'"origin":"Unknown",' +
+			'"race":"Primordial",' +
+			'"desc":""' +
+		'},' +
+
+		// Dibella
+		'{' +
+			'"name":"Dibella",' +
+			'"file":"dibella",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Sunem",' +
+			'"race":"Tiefling",' +
+			'"desc":""' +
+		'},' +
+
+		// Draedon
+		'{' +
+			'"name":"Draedon",' +
+			'"file":"draedon",' +
+			'"fact":"demanatus",' +
+			'"status":"Deceased",' +
+			'"origin":"Barindale",' +
+			'"race":"Warforged",' +
+			'"desc":""' +
+		'},' +
+
+		// Dragon Folly
+		'{' +
+			'"name":"Dragon Folly",' +
+			'"file":"dragonfolly",' +
+			'"fact":"unaligned",' +
+			'"status":"Alive",' +
+			'"origin":"Triven Lab",' +
+			'"race":"Jungle Dragon",' +
+			'"desc":""' +
+		'},' +
+
+		// Elros
+		'{' +
+			'"name":"Elros",' +
+			'"file":"elros",' +
+			'"fact":"unaligned",' +
+			'"status":"Alive",' +
+			'"origin":"Jungle",' +
+			'"race":"Half Elf",' +
+			'"desc":""' +
+		'},' +
+
+		// Emmet
+		'{' +
+			'"name":"Emmet",' +
+			'"file":"emmet",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Lothra",' +
+			'"race":"Wood Elf",' +
+			'"desc":""' +
+		'},' +
+
+		// Eramis
+		'{' +
+			'"name":"Eramis",' +
+			'"file":"eramis",' +
+			'"fact":"resistance",' +
+			'"status":"Deceased",' +
+			'"origin":"Unknown",' +
+			'"race":"Elven Goddess",' +
+			'"desc":""' +
+		'},' +
+
+		// Gnomeo
+		'{' +
+			'"name":"Gnomeo",' +
+			'"file":"gnomeo",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Unknown",' +
+			'"race":"Gnome",' +
+			'"desc":""' +
+		'},' +
+
+		// Goravan
+		'{' +
+			'"name":"Goravan",' +
+			'"file":"goravan",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Ponem",' +
+			'"race":"Goliath",' +
+			'"desc":""' +
+		'},' +
+
+		// Jake
+		'{' +
+			'"name":"Jake",' +
+			'"file":"jake",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Lothra",' +
+			'"race":"High Elf",' +
+			'"desc":""' +
+		'},' +
+
+		// Johnathon
+		'{' +
+			'"name":"Johnathon",' +
+			'"file":"johnathon",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Barindale",' +
+			'"race":"Human",' +
+			'"desc":""' +
+		'},' +
+
+		// Joseph
+		'{' +
+			'"name":"Joseph Banana",' +
+			'"file":"joseph",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Keotor",' +
+			'"race":"Dwarf",' +
+			'"desc":""' +
+		'},' +
+
+		// Jungle Guards
+		'{' +
+			'"name":"Jungle Guards",' +
+			'"file":"jungle-guard",' +
+			'"fact":"demanatus",' +
+			'"status":"Alive",' +
+			'"origin":"Jungle",' +
+			'"race":"Human",' +
+			'"desc":""' +
+		'},' +
+
+		// Kristine
+		'{' +
+			'"name":"Kristine",' +
+			'"file":"kristine",' +
+			'"fact":"resistance",' +
+			'"status":"Deceased",' +
+			'"origin":"Marione",' +
+			'"race":"Elf",' +
+			'"desc":""' +
+		'},' +
+
+		// Kronos
+		'{' +
+			'"name":"Kronos",' +
+			'"file":"kronos",' +
+			'"fact":"godly",' +
+			'"status":"Deceased",' +
+			'"origin":"Unknown",' +
+			'"race":"Dwarven God",' +
+			'"desc":""' +
+		'},' +
+
+		// Leviathan
+		'{' +
+			'"name":"Leviathan",' +
+			'"file":"leviathan",' +
+			'"fact":"leviathan",' +
+			'"status":"Alive",' +
+			'"origin":"Ponem",' +
+			'"race":"Goliath",' +
+			'"desc":""' +
+		'},' +
+
+		// Lilith
+		'{' +
+			'"name":"Lilith",' +
+			'"file":"lilith",' +
+			'"fact":"unaligned",' +
+			'"status":"Deceased",' +
+			'"origin":"Marione",' +
+			'"race":"Elf",' +
+			'"desc":""' +
+		'},' +
+
+		// Marka
+		'{' +
+			'"name":"Marka",' +
+			'"file":"marka",' +
+			'"fact":"demanatus",' +
+			'"status":"Deceased",' +
+			'"origin":"Underdark",' +
+			'"race":"Mind Flayer",' +
+			'"desc":""' +
+		'},' +
+
+		// Nao
+		'{' +
+			'"name":"Nao",' +
+			'"file":"nao",' +
+			'"fact":"unaligned",' +
+			'"status":"Alive",' +
+			'"origin":"Obora",' +
+			'"race":"Owlbear",' +
+			'"desc":""' +
+		'},' +
+
+		// Obadiah
+		'{' +
+			'"name":"Obadiah",' +
+			'"file":"oni",' +
+			'"fact":"pyranus",' +
+			'"status":"Deceased",' +
+			'"origin":"Pyranus",' +
+			'"race":"Oni",' +
+			'"desc":""' +
+		'},' +
+
+		// Okraz
+		'{' +
+			'"name":"Okraz",' +
+			'"file":"okraz",' +
+			'"fact":"godly",' +
+			'"status":"Alive",' +
+			'"origin":"Regen",' +
+			'"race":"Familiar",' +
+			'"desc":""' +
+		'},' +
+
+		// Pyranus
+		'{' +
+			'"name":"Pyranus",' +
+			'"file":"pyranus",' +
+			'"fact":"pyranus",' +
+			'"status":"Deceased",' +
+			'"origin":"Unknown",' +
+			'"race":"Elven God",' +
+			'"desc":""' +
+		'},' +
+
+		// Rainey
+		'{' +
+			'"name":"Rainey",' +
+			'"file":"rainey",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Obora",' +
+			'"race":"Elf",' +
+			'"desc":""' +
+		'},' +
+
+		// Rapheal
+		'{' +
+			'"name":"Rapheal",' +
+			'"file":"rapheal",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Obora",' +
+			'"race":"Shifter",' +
+			'"desc":""' +
+		'},' +
+
+		// Regen
+		'{' +
+			'"name":"Regen",' +
+			'"file":"regen",' +
+			'"fact":"godly",' +
+			'"status":"Deceased",' +
+			'"origin":"Unknown",' +
+			'"race":"Elven Goddess",' +
+			'"desc":""' +
+		'},' +
+
+		// Semuanya
+		'{' +
+			'"name":"Semuanya",' +
+			'"file":"semuanya",' +
+			'"fact":"godly",' +
+			'"status":"Alive",' +
+			'"origin":"Unknown",' +
+			'"race":"Lizardfolk God",' +
+			'"desc":""' +
+		'},' +
+
+		// Stallion
+		'{' +
+			'"name":"Stallion",' +
+			'"file":"stallion",' +
+			'"fact":"leviathan",' +
+			'"status":"Alive",' +
+			'"origin":"Ponem",' +
+			'"race":"Goliath",' +
+			'"desc":""' +
+		'},' +
+
+		// Storm Weaver
+		'{' +
+			'"name":"Storm Weaver",' +
+			'"file":"stormweaver",' +
+			'"fact":"godly",' +
+			'"status":"Alive",' +
+			'"origin":"Unknown",' +
+			'"race":"Dwarven God",' +
+			'"desc":""' +
+		'},' +
+
+		// Valkyries
+		'{' +
+			'"name":"Valkyries",' +
+			'"file":"valkyrie",' +
+			'"fact":"pyranus",' +
+			'"status":"Disbanded",' +
+			'"origin":"Various",' +
+			'"race":"Various",' +
+			'"desc":""' +
+		'},' +
+
+		// Varion
+		'{' +
+			'"name":"Varion",' +
+			'"file":"varion",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Marione",' +
+			'"race":"Half Elf",' +
+			'"desc":"resistance"' +
+		'},' +
+
+		// Victor
+		'{' +
+			'"name":"Victor",' +
+			'"file":"victor",' +
+			'"fact":"resistance",' +
+			'"status":"Alive",' +
+			'"origin":"Triven",' +
+			'"race":"Warforged",' +
+			'"desc":""' +
+		'},' +
+
+		// Voramir
+		'{' +
+			'"name":"Voramir",' +
+			'"file":"voramir",' +
+			'"fact":"demanatus",' +
+			'"status":"Deceased",' +
+			'"origin":"Sunem",' +
+			'"race":"Desert Deity",' +
+			'"desc":""' +
+		'},' +
+
+		// Warden
+		'{' +
+			'"name":"The Warden",' +
+			'"file":"warden",' +
+			'"fact":"unaligned",' +
+			'"status":"Deceased",' +
+			'"origin":"Unknown",' +
+			'"race":"Primordial",' +
+			'"desc":""' +
+		'},' +
+
+		// Xanter
+		'{' +
+			'"name":"Xanter",' +
+			'"file":"xanter",' +
+			'"fact":"godly",' +
+			'"status":"Alive",' +
+			'"origin":"Unknown",' +
+			'"race":"Elven Goddess",' +
+			'"desc":""' +
+		'},' +
+
+		// Yharim
+		'{' +
+			'"name":"Yharim",' +
+			'"file":"yharim",' +
+			'"fact":"unaligned",' +
+			'"status":"Deceased",' +
+			'"origin":"Jungle",' +
+			'"race":"Human",' +
+			'"desc":""' +
+		'},' +
+
+		// Yharon
+		'{' +
+			'"name":"Yharon",' +
+			'"file":"yharon",' +
+			'"fact":"demanatus",' +
+			'"status":"Alive",' +
+			'"origin":"Jungle",' +
+			'"race":"Jungle Dragon",' +
+			'"desc":""' +
+		'}]}';
